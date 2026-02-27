2c81bb607125b0ffb6eebbeed4a1f558f5bcc4a0
saad <saadnadeemch999@gmail.com>
2026-02-28 00:03:10 +0500

    okok

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,20 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , CommonModule , RouterModule],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
   isDark = false;
+
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    if (this.isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 }
